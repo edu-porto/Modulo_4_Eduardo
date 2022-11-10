0efc1e8d54898b40397f53f2351eb639a1e2edbb
edu-porto <eduardo.porto@sou.inteli.edu.br>
2022-11-10 14:43:01 -0300

    fiz ex 5

--- SEMANA_01/EXERCICIOS/ex_5/main.cpp
+++ SEMANA_01/EXERCICIOS/ex_5/main.cpp
@@ -1,1 +1,27 @@
-// 5 - Faça uma função que pergunta ao usuário se ele deseja continuar o mapeamento e retorna verdadeiro ou falso
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int continuar() {
+  int inputUser;
+  printf("Deseja parar o mapeamento ? Digite 1 para sim ou 0 para não ");
+  cin >> inputUser;
+  return inputUser;
+}
+
+// Descomente a função main abaixo para testar o exercício 5
+
+int main() {
+  int parar = 0;
+
+  while (parar == 0) {
+    parar = continuar();
+  }
+  // A função deve perguntar: "Digite 1 para parar o mapeamento!"
+  // Se o usuário digitar 1, deve sair do loop while declarado acima
+  // Em caso contrário, continua no loop acima e pergunta novamente
+
+  return 1;
+}
